4fa2a7a42fdccaf53e6c88379c3d86ef897911f8
SofiaRan74 <sofiaran024@gmail.com>
2025-02-26 14:23:56 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -26,6 +26,7 @@ app.config(function ($routeProvider, $locationProvider) {
     .when("/calificaciones", {
         templateUrl: "/calificaciones",
         controller: "calificacionesCtrl"
+    })
     .otherwise({
         redirectTo: "/"
     })
